5488624c18368fa4dc57a7c25f78dc32ed65507c
Leonardo Tolotti <leotolotti@hotmail.com>
2023-04-01 18:29:40 -0300

    first commit

--- assets/css/Css.js
+++ assets/css/Css.js
@@ -25,5 +25,42 @@ const css = StyleSheet.create({
     width: 120,
     height: 120,
   },
+  darkbg: {
+    backgroundColor: "#333",
+  },
+  login__logomarca: {
+    marginBottom: 30,
+  },
+  login__logomarca_img: {
+    width: 120,
+    height: 120,
+  },
+  login__msg: (text = "none") => ({
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "red",
+    marginBottom: 15,
+    display: text,
+  }),
+  login__form: {
+    width: "80%",
+  },
+  login__input: {
+    backgroundColor: "#fff",
+    fontSize: 19,
+    padding: 7,
+    marginBottom: 15,
+  },
+  login__button: {
+    padding: 12,
+    backgroundColor: "#F58634",
+    alignSelf: "center",
+    borderRadius: 5,
+  },
+  login__buttonText: {
+    fontWeight: "bold",
+    fontSize: 22,
+    color: "#333",
+  },
 });
 export { css };
--- views/Login.js
+++ views/Login.js
@@ -1,10 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  Text,
+  View,
+} from "react-native";
+import { css } from "../assets/css/Css";
 
 export default function Login() {
+  const [display, setDisplay] = useState("none");
   return (
-    <View>
-      <Text>Esse é o componente Login do sistema da LottiLog</Text>
-    </View>
+    //KeyboardAvoidingView não deixa o teclado subir em cima dos componentes
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={[css.container, css.darkbg]}
+    >
+      <View style={css.login__logomarca}>
+        <Image
+          style={css.login__logomarca_img}
+          source={require("../assets/img/logomarca.png")}
+        />
+      </View>
+      <View>
+        <Text style={css.login__msg(display)}>Usuário ou senha inválidos!</Text>
+      </View>
+      <View style={css.login__form}>
+        <TextInput style={css.login__input} placeholder="Usuário:" />
+        <TextInput
+          style={css.login__input}
+          placeholder="Senha:"
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={css.login__button}
+          onPress={() => setDisplay("flex")}
+        >
+          <Text style={css.login__buttonText}>Entrar</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
