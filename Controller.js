f3c16b394c38b9c922ae90c57fc7db630b7a9611
Leonardo Tolotti <leotolotti@hotmail.com>
2023-04-02 10:34:12 -0300

    inicio Backend

--- Controller.js
+++ Controller.js
@@ -1,13 +1,67 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const models = require("./models");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
+let user = models.User;
+let tracking = models.Tracking;
+let product = models.Product;
 
-app.get("/", (req, res) => {
-  res.send("Meu servidor backend já está rodando!");
+app.get("/create", async (req, res) => {
+  let create = await user.create({
+    name: "leonardo",
+    password: "abc",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send("Usuário criado com sucesso!");
+});
+app.get("/createtracking", async (req, res) => {
+  let create = await tracking.create({
+    code: "123456789BR",
+    local: "Farroupilha",
+    userId: "2",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send("Tracking criado com sucesso!");
+});
+app.get("/createproduct", async (req, res) => {
+  let create = await product.create({
+    name: "Bola",
+    trackingId: "1",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
+  res.send("Produto criado com sucesso!");
+});
+
+app.get("/read", async (req, res) => {
+  let read = await user.findAll({
+    raw: true,
+  });
+  console.log(read);
+  res.send("Busca realizada!");
+});
+
+app.get("/update", async (req, res) => {
+  let update = await user
+    .findByPk(2, { include: [{ all: true }] })
+    .then((response) => {
+      response.Trackings[0].local = "Farroupilha-RS";
+      response.Trackings[0].save();
+    });
+  res.send("Update realizada!");
+});
+
+app.get("/delete", async (req, res) => {
+  user.destroy({
+    where: { id: 2 },
+  });
+  res.send("Delete realizado!");
 });
 
 let port = process.env.PORT || 3000;
